ec57cb3e83a1117157ce247ca4b24e60cbe70f5c
GustavoGogola91 <90578528+GustavoGogola91@users.noreply.github.com>
2024-03-07 23:52:40 -0300

    ExAula03

--- ExAula03/src/Exercicios.java
+++ ExAula03/src/Exercicios.java
@@ -5,10 +5,12 @@ public class Exercicios {
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        ex01();
+        ex04();
     }
 
     public static void ex01() {
+
+        System.out.println("EXERCICIO 1");
 
         int[] arr = new int[5];
         int med = 0;
@@ -42,6 +44,77 @@ public class Exercicios {
         for(int i = 0; i < arr.length; i++) {
             if(arr[i] == med) {
                 System.out.printf("%d ", arr[i]);
+            }
+        }
+    }
+
+    public static void ex02() {
+
+        System.out.println("EXERCICIO 2");
+        
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Digite o numero para a posição %d: ", i);
+            arr[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Posição %d: %d - ", i, arr[i]);
+
+            if(arr[i] > 0) {
+                System.out.println("POSITIVO");
+            } else if(arr[i] < 0) {
+                System.out.println("NEGATIVO");
+            } else {
+                System.out.println("ZERO");
+            }
+        }
+    }
+
+    public static void ex03() {
+
+        System.out.println("EXERCICIO 3");
+
+        int[] arr = new int[5];
+        int[] dobro = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Digite o numero para a posição %d: ", i);
+            arr[i] = input.nextInt();
+            dobro[i] = arr[i] * 2;
+        }
+
+        System.out.println("Array original");
+        for (int i = 0; i < dobro.length; i++) {
+            System.out.printf("Posição %d: %d%n",i, arr[i]);
+        }
+        
+        System.out.println("Array dobro");
+        for (int i = 0; i < dobro.length; i++) {
+            System.out.printf("Posição %d: %d%n",i, dobro[i]);
+        }
+    }
+
+    public static void ex04() {
+        
+        System.out.println("EXERCICIO 4");
+
+        int[] arr = new int[5];
+        int n;
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Digite o numero para a posição %d: ", i);
+            arr[i] = input.nextInt();
+        }
+
+        System.out.print("Informe um valor qualquer: ");
+        n = input.nextInt();
+
+        System.out.printf("Indice dos numeros menores que %d:%n", n);
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < n) {
+                System.out.println(i);
             }
         }
 
