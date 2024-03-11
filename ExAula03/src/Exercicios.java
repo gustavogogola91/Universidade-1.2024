b5e47ef0c172d3e971fba088dd398717a8cc9ad3
Gustavo Gogola <90578528+gustavogogola91@users.noreply.github.com>
2024-03-11 19:46:58 -0300

    Add files via upload

--- ExAula03/src/Exercicios.java
+++ ExAula03/src/Exercicios.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Exercicios {
-    
+
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        ex04();
+        ex08();
     }
 
     public static void ex01() {
@@ -16,7 +16,7 @@ public class Exercicios {
         int med = 0;
 
         System.out.println("Informe 5 numeros: ");
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.printf("%nNumero %d: ", i + 1);
             arr[i] = input.nextInt();
             med += arr[i];
@@ -27,22 +27,22 @@ public class Exercicios {
         System.out.printf("A média destes valores é: %d%n", med);
 
         System.out.print("Números maiores que a média: ");
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] > med) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > med) {
                 System.out.printf("%d ", arr[i]);
             }
         }
         System.out.println();
         System.out.print("Números menores que a média: ");
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] < med) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < med) {
                 System.out.printf("%d ", arr[i]);
             }
         }
         System.out.println();
         System.out.print("Números iguais que a média: ");
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == med) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == med) {
                 System.out.printf("%d ", arr[i]);
             }
         }
@@ -51,7 +51,7 @@ public class Exercicios {
     public static void ex02() {
 
         System.out.println("EXERCICIO 2");
-        
+
         int[] arr = new int[5];
 
         for (int i = 0; i < arr.length; i++) {
@@ -62,9 +62,9 @@ public class Exercicios {
         for (int i = 0; i < arr.length; i++) {
             System.out.printf("Posição %d: %d - ", i, arr[i]);
 
-            if(arr[i] > 0) {
+            if (arr[i] > 0) {
                 System.out.println("POSITIVO");
-            } else if(arr[i] < 0) {
+            } else if (arr[i] < 0) {
                 System.out.println("NEGATIVO");
             } else {
                 System.out.println("ZERO");
@@ -87,17 +87,17 @@ public class Exercicios {
 
         System.out.println("Array original");
         for (int i = 0; i < dobro.length; i++) {
-            System.out.printf("Posição %d: %d%n",i, arr[i]);
+            System.out.printf("Posição %d: %d%n", i, arr[i]);
         }
-        
+
         System.out.println("Array dobro");
         for (int i = 0; i < dobro.length; i++) {
-            System.out.printf("Posição %d: %d%n",i, dobro[i]);
+            System.out.printf("Posição %d: %d%n", i, dobro[i]);
         }
     }
 
     public static void ex04() {
-        
+
         System.out.println("EXERCICIO 4");
 
         int[] arr = new int[5];
@@ -113,7 +113,7 @@ public class Exercicios {
 
         System.out.printf("Indice dos numeros menores que %d:%n", n);
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] < n) {
+            if (arr[i] < n) {
                 System.out.println(i);
             }
         }
@@ -121,12 +121,11 @@ public class Exercicios {
 
     public static void ex05() {
 
-
         System.out.println("EXERCICIO 05");
 
         int[] arr = new int[5];
         int n;
-        int cont;
+        int cont = 0;
 
         for (int i = 0; i < arr.length; i++) {
             System.out.printf("Digite o numero para a posição %d: ", i);
@@ -134,10 +133,10 @@ public class Exercicios {
         }
 
         System.out.print("Digite um numero qualquer: ");
-        input.nextInt();
+        n = input.nextInt();
 
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == n) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == n) {
                 cont++;
             }
         }
@@ -145,5 +144,107 @@ public class Exercicios {
         System.out.printf("O número %d apareceu %d vez(es) no array", n, cont);
     }
 
-    
+    public static void ex06() {
+
+        float[] notas = new float[5];
+        float[] pesos = new float[5];
+        float med = 0;
+        float div = 0;
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.printf("Digite o numero para a nota %d: ", i);
+            notas[i] = input.nextFloat();
+        }
+
+        for (int i = 0; i < pesos.length; i++) {
+            System.out.printf("Digite o numero para o peso %d: ", i);
+            pesos[i] = input.nextFloat();
+        }
+
+        for (int i = 0; i < notas.length; i++) {
+            med += (notas[i] * pesos[i]);
+            div += pesos[i];
+        }
+
+        med = med / div;
+
+        System.out.printf("A média é %.2f%n", med);
+    }
+
+    public static void ex07() {
+
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[5];
+        boolean equals = true;
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.printf("Digite o numero para a posição %d do array 1: ", i);
+            arr1[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.printf("Digite o numero para a posição %d do array 2: ", i);
+            arr2[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            if(arr1[i] != arr2[i]) {
+                equals = false;
+                 break;
+            }
+        }
+
+        if(equals) {
+            System.out.println("Os dois arrays são iguais.");
+        } else {
+            System.out.println("Os dois arrays são diferentes.");
+        }
+    }
+
+    public static void ex08() {
+        
+        int[] A = new int[5];
+        int[] B = new int[5];
+        int[] C = new int[10];
+
+        for (int i = 0; i < A.length; i++) {
+            System.out.printf("Digite o numero para a posição %d do array A: ", i);
+            A[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < B.length; i++) {
+            System.out.printf("Digite o numero para a posição %d do array B: ", i);
+            B[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < C.length; i++) {
+
+            if(i < 5) {
+                C[i] = A[i];
+            } else {
+                C[i] = B[i - 5];
+            }
+            
+        }
+        System.out.print("A: ");
+        for (int i = 0; i < A.length; i++) {
+            System.out.printf("%d ", A[i]);
+        }
+        
+        System.out.print("\nB: ");
+        for (int i = 0; i < B.length; i++) {
+            System.out.printf("%d ", B[i]);
+        }
+
+        System.out.print("\nC: ");
+        for (int i = 0; i < C.length; i++) {
+            System.out.printf("%d ", C[i]);
+        }
+        
+    }
+
+    public static void ex09() {
+        
+    }
+
 }
